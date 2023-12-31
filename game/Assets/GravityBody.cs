494de5ede711bf3254666c0976106e96e89ee3b1
ErikWe <erikwestberg7@gmail.com>
2023-12-31 02:43:18 +0100

    Limit the magnitude of the force applied to objects

--- game/Assets/GravityBody.cs
+++ game/Assets/GravityBody.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,11 @@ using UnityEngine.SceneManagement;
 public static class Constants
 {
     public static float gravityConstant = 5;
+}
+
+public static class PhysicsSettings
+{
+    public static float MaxForce = 25;
 }
 
 [RequireComponent(typeof(Rigidbody))]
@@ -42,10 +48,21 @@ public class GravityBody : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 force = Vector3.zero;
+
         foreach (GravityBody other in others)
         {
-            force += (other.rigidBody.position - rigidBody.position) * other.mass / Mathf.Pow(Vector3.Distance(rigidBody.position, other.rigidBody.position), 2);
+            var distance = Vector3.Distance(rigidBody.position, other.rigidBody.position);
+
+            if (distance is 0)
+            {
+                continue;
+            }
+
+            force += (other.rigidBody.position - rigidBody.position) * other.mass / Mathf.Pow(distance, 2);
         }
+
+        force = Vector3.ClampMagnitude(force, PhysicsSettings.MaxForce);
+
         rigidBody.AddForce(mass * Constants.gravityConstant * force);
     }
 }
